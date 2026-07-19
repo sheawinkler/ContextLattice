b42ae14030ad9bbba705ba348fb40848d8b3f5b3
Shea Winkler <[email protected]>
2026-07-18 23:01:34 -0600

    fix(installer): link Frontier policy and agent fit CLIs

--- scripts/install_global_agent_tools.sh
+++ scripts/install_global_agent_tools.sh
@@ -686,6 +686,8 @@ GO_NATIVE_COMMANDS=(
   contextlattice_synthesis_pack_v2
   contextlattice_retrieval_plan
   contextlattice_retrieval_governance
+  contextlattice_policy_lab
+  contextlattice_agent_fit
   contextlattice_claim_write
   contextlattice_claim_query
   contextlattice_continuity_reconcile
